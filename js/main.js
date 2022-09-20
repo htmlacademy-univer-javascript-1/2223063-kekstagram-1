6db38a905ed5cf8c5d9a713101c29245a8585f7f
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-09-20 20:04:08 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-function getRandomNumber(first, second){
-  if (first > second || first < 0 || second < 0){
-    return new Error('Error. Change input values')
-    }
-  return (Math.round(Math.random() * (second - first) + first))
-}
 
-function checkLength(string, length){
-  return(string.length <= length)
-}
