6d4024124b028de1d13e3a15c7822f2db93da8d1
Keks <keks@htmlacademy.ru>
2022-09-20 21:11:08 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,13 @@
+function getRandomNumber(first, second){
+  if (first > second || first < 0 || second < 0){
+    return new Error('Error. Change input values');
+  }
+  return (Math.round(Math.random() * (second - first) + first));
+}
+
+function checkLength(string, length){
+  return(string.length <= length);
+}
+
+getRandomNumber(1, 4);
+checkLength('123', 3);
