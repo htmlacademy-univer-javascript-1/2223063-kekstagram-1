00bb963351878143bf70352b41d74ef70450a36f
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-10-03 21:48:04 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomNumber(first, second){
   const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
-  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)))
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
