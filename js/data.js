96eb76d494cf81b3d8997095d7e865d76ee5ecbf
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-10-18 21:10:56 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,9 +1,5 @@
 import { getRandomNumber } from './util.js';
 
-const getRandomArrayElement = function (elements) {
-  return elements[getRandomNumber(0, elements.length - 1)];
-};
-
 const NAMES = [
   'Алексей',
   'Виктор',
@@ -30,6 +26,10 @@ const DESCRIPTIONS  = [
 
 const PHOTO_COUNT = 25;
 const PHOTO_DESCRIPTION = [];
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
 
 for (let i = 1; i <= PHOTO_COUNT; i++) {
   PHOTO_DESCRIPTION.push({
