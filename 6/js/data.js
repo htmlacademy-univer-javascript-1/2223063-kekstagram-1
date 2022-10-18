3806058644d7efa2d2774a9d445b8828d668bcd0
Keks <keks@htmlacademy.ru>
2022-10-18 19:11:28 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,48 @@
+import { getRandomNumber } from './util.js';
+
+const NAMES = [
+  'Алексей',
+  'Виктор',
+  'Василиса',
+  'Василий',
+  'Валентин',
+  'Анна'
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Пойдет и ладно.',
+  'Нормально',
+  'Чудесно все'
+];
+
+const DESCRIPTIONS  = [
+  'Это просто доказательство того, что я могу быть лучшим эгоистом, чем вы.',
+  'Я очень творческая личность — хочу творю, хочу вытворяю!',
+  'Дружба — это один разум в двух телах',
+  'Закон подлости — это единственный закон, который реально работает в нашей стране!'
+];
+
+const PHOTO_COUNT = 25;
+const PHOTO_DESCRIPTION = [];
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
+
+for (let i = 1; i <= PHOTO_COUNT; i++) {
+  PHOTO_DESCRIPTION.push({
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: DESCRIPTIONS[i],
+    likes: getRandomNumber(15, 200),
+    comments: [{
+      id: i * getRandomNumber(1, 100),
+      avatar: `img/avatar${getRandomNumber(1, 6)}.svg`,
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAMES)
+    }]
+  });
+}
+export { NAMES, MESSAGES, DESCRIPTIONS, PHOTO_DESCRIPTION };
