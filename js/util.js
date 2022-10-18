80630b5bcf5024e4a09b3612ba0a886600db9a84
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-10-18 19:45:53 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,1 +1,14 @@
+function getRandomNumber(first, second){
+  const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
 
+function checkStringLength (string, length) {
+  return string.length <= length;
+};
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
+export { getRandomNumber, checkStringLength };
