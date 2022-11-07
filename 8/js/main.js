3c145407da29823f5a27f3349c4d4fae532e271e
Keks <keks@htmlacademy.ru>
2022-11-07 10:48:23 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,5 @@
+import { NAMES, MESSAGES, DESCRIPTIONS, PHOTO_DESCRIPTION } from './data.js';
+console.log(NAMES);
+console.log(MESSAGES);
+console.log(DESCRIPTIONS);
+console.log(PHOTO_DESCRIPTION);
