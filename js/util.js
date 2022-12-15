1650a06748cc020aae6cbccf87eb94252f37eeb9
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-12-15 19:34:10 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -8,6 +8,26 @@ function checkStringLength (string, length) {
   return string.length <= length;
 }
 const isEscape = (evt) => evt.key === 'Escape';
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.classList.add('error_message');
+  alertContainer.textContent = message;
 
+  document.body.append(alertContainer);
 
-export { getRandomNumber, checkStringLength, isEscape };
+  setTimeout(() => {
+    alertContainer.remove();
+
+  }, ALERT_SHOW_TIME);
+};
+
+export { getRandomNumber, checkStringLength, isEscape, showAlert };
