9b96cd2490ffc60451134570be64960a4c9812b7
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-12-15 19:35:18 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ import './hashtag.js';
 import './uploadFile.js';
 import './photoEffects.js';
 import './photoScaling.js';
+import './server.js';
+
+import { renderPhotos } from './rendering.js';
+import { sendRequest } from './server.js';
+import { showAlert } from './util.js';
+
+const onSuccess = (data) => {
+  renderPhotos(data);
+};
+
+const onFail = (error) =>{
+  showAlert(error);
+};
+
+const method = 'GET';
+
+sendRequest(onSuccess, onFail, method);
